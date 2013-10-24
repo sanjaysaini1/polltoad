f572387657ba09728c6d367b9cbd06cad647d24f
sanjaysaini1 <sanjaysaini1@gmail.com>
2013-10-25 02:31:49 +0530

    adding the sqls

---
+++ sql/UT.sql
@@ -0,0 +1,7 @@
+INSERT INTO state( name, ut) VALUES ('Andaman and Nicobar Islands',1);
+INSERT INTO state( name, ut) VALUES ('Chandigarh',1);
+INSERT INTO state( name, ut) VALUES ('Dadra and Nagar Haveli',1);
+INSERT INTO state( name, ut) VALUES ('Daman and Diu',1);
+INSERT INTO state( name, ut) VALUES ('Delhi',1);
+INSERT INTO state( name, ut) VALUES ('Lakshdweep',1);
+INSERT INTO state( name, ut) VALUES ('Puducherry',1);
