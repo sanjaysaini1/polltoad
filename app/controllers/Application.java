956b67802bc7ab0f78e6818122a6ee32bfddef5b
sanjaysaini1 <sanjaysaini1@gmail.com>
2014-04-06 02:03:58 +0530

    Refactoring the Application Controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,13 +10,6 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
-
-	static Form<Politician> politicianForm = Form.form(Politician.class);
-	static Form<State> stateForm = Form.form(State.class);
-	static Form<District> districtForm = Form.form(District.class);
-	static Form<PoliticalParty> politicalpartyForm = Form.form(PoliticalParty.class);
-	static Form<Constituency> constituencyForm = Form.form(Constituency.class);
-
 	
 	public static Result index() {
 		return redirect(routes.PoliticianController.politicians());
